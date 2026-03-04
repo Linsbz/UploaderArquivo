80643a1cc8308bda59efc2ce2d555aa2c113f2f4
Linsbz <lincolnbazoni1@gmail.com>
2026-03-04 14:18:31 -0300

    Initial commit - API Uploader Arquivos

---
+++ UploaderArquivos/Controllers/UploadController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FileUploaderApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UploadController : ControllerBase
+    {
+        [HttpPost("upload")]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return BadRequest("Arquivo inválido.");
+
+            var uploadsPath = Path.Combine(Directory.GetCurrentDirectory(), "Uploads");
+
+            if (!Directory.Exists(uploadsPath))
+                Directory.CreateDirectory(uploadsPath);
+
+            var filePath = Path.Combine(uploadsPath, file.FileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return Ok(new
+            {
+                message = "Upload realizado com sucesso",
+                fileName = file.FileName
+            });
+        }
+    }
+}
---
+++ UploaderArquivos/Program.cs
@@ -0,0 +1,39 @@
+
+namespace UploaderArquivos
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.WebHost.ConfigureKestrel(options =>
+            {
+                options.ListenAnyIP(5000);
+            });
+
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            //app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
